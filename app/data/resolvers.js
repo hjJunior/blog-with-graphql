3ea4e9fa22bb2841f329773b8e8a1be2ad026935
Helio S. Junior <hj_junior@hotmail.com>
2018-09-11 08:28:31 -0300

    Starting building resolvers

--- app/data/resolvers.js
+++ app/data/resolvers.js
@@ -1,15 +1,55 @@
 'use strict'
 
+const User = use('App/Models/User')
+const Post = use('App/Models/Post')
+const slugify = require('slugify')
+
 const resolvers = {
   Query: {
-    async allUsers() {},
-    async fetchUser(_, { id }) {},
-    async allPosts() {},
-    async fetchPost(_, { id }) {}
+    async allUsers() {
+      const users = await User.all()
+      return users.toJSON()
+    },
+    async fetchUser(_, { id }) {
+      const user = await User.find(id)
+      return user.toJSON()
+    },
+    async allPosts() {
+      const posts = await Post.all()
+      return posts.toJSON()
+    },
+    async fetchPost(_, { id }) {
+      const post = await Post.find(id)
+      return post.toJSON()
+    }
   },
   Mutation: {
-    async login(_, { email, password }, { auth }) {},
-    async createUser(_, { username, email, password }) {},
-    async addPost(_, { title, content }, { auth }) {}
+    async login(_, { email, password }, { auth }) {
+      const { token } = await auth.attempt(email, password)
+      return token
+    },
+    async createUser(_, { username, email, password }) {
+      return await User.create({ username, email, password })
+    },
+    async addPost(_, { title, content }, { auth }) {
+      try {
+        // Check if user is logged in
+        await auth.check()
+
+        // Get the authenticated user
+        const user = await auth.getUser()
+
+        // Add new post
+        return await Post.create({
+          user_id: user.id,
+          title,
+          slug: slugify(title, { lower: true }),
+          content
+        })
+      } catch (error) {
+        // Throw error if user is not authenticated
+        throw new Error('Missing or invalid jwt token')
+      }
+    }
   }
 }
